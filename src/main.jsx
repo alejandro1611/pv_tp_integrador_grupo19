c0ac9db493d9e27a49758a7986cf009e8b9d0c9b
alejandro1611 <cruzimanol944@gmail.com>
2026-06-20 18:16:37 -0300

    Agrego carpeta css y estilo al footer - Cruz Alejandro

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AdminProvider } from "./context/AdminContext";
 import router from "./routes/AppRouter";
+import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
